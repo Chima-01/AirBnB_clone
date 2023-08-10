058585680463d5f1665e3861f6f18bacd72d4e27
Chima-01 <divinechima76@gmail.com>
2023-08-10 03:29:42 -0700

    made changes to the basemodel

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """File needed to create a package of the models(classes)"""
 """Creates unique file storage system for project"""
-from models.engine.file_storage import FileStorage
+#from models.engine.file_storage import FileStorage
 
-file_storage = FileStorage()
-file_storage.reload()
+#file_storage = FileStorage()
+#file_storage.reload()

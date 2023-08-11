f454a487d6be08151ab6ec2b8638b3f38ae44a8d
Chima-01 <divinechima76@gmail.com>
2023-08-11 05:36:15 -0700

    working on file storage module

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
-""" This class filestorageserializes instances to a JSON file
-    and deserializes JSON file to instances"""
+import json
+import os.path
+""" class FileStorage serializes instances to a JSON file
+    and deserializes JSON file to instances """
 
 
 class FileStorage:
-    """
-        Attributes:
-            args:
+    """ Initialize a private class attribute """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """ Returns private attribute object """
+        return self.__objects
+
+    def new(self, obj):
+        """ makes key with obj class and id """
+        ob_name = obj.__class.__name__
+        key = "{}.{}".format(ob_name, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """ serialize objects to json file """

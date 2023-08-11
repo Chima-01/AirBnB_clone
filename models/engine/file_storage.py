ddd10c658f2254a8f9ab65950a1f791b0d134ee1
Chima-01 <divinechima76@gmail.com>
2023-08-10 23:53:17 -0700

    fixed class name bug in base class module

---
+++ models/engine/file_storage.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This class filestorageserializes instances to a JSON file
+    and deserializes JSON file to instances"""
+
+
+class FileStorage:
+    """
+        Attributes:
+            args:

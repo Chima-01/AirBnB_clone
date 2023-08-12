0623f55855301e2df9a9f7ac7880f1c71fe9d3d3
Serinajefthas <serina.jefthas@gmail.com>
2023-08-12 05:08:13 -0700

    Create new FileStorage instance

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """File needed to create a package of the models(classes)"""
 """Creates unique file storage system for project"""
+
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
